bb929a6bbbcfb0b04ad41451b230e0e005810cf0
esplito <eesplund@gmail.com>
2018-05-02 15:08:07 +0200

    Added more html

--- app/index.php
+++ app/index.php
@@ -2,9 +2,17 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Home</title>
+	<title>Login Page</title>
 </head>
 <body>
-	
+	<div class="col__large-5">
+		<div class="side-image">
+			<h1 class="side-image__title">Commenter</h1>
+			<h4 class="side-image__subtitle">The place where you can comment on anything</h4>
+		</div>
+	</div>
+	<div class="col__large-7">
+		
+	</div>
 </body>
 </html>
