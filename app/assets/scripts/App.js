212dea544c2ab79a667303565f8fff9381d88df7
Emil Esplund <eesplund@gmail.com>
2018-05-17 12:37:57 +0200

    Completed positioning of header. Added textarea for postfeed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,91 +1,97 @@
-function getById (id) {return document.getElementById(id)};
+$(document).ready(function() {
+	function getById (id) {return document.getElementById(id)};
 
-function validateForm(form_type, form_e){
-	switch(form_type){
-		case "login-form":
-				var userLogin = {};
-				userLogin.email = form_e.elements["email"].value;
-				userLogin.pw = form_e.elements["password"].value;
-				if(validateEmail(userLogin.email) === true){
+	$('textarea').keypress( function(e){
+		if($(this)[0].scrollHeight > 100){
+			$(this).val($(this).val().replace(/[\r\n\v]$/g, ''));
+			return false;
+		}
+	});
 
-					if(userLogin.pw != ""){
-						//check pw and email in db
-						$.ajax({
-					       type: "POST",
-					       url: '../include/login-validation.php',
-					       data: {
-					       	reqType: "login",
-					       	email: userLogin.email,
-  							password: userLogin.pw 
-					       },
-					       success: function(response)
-					       {
-					          if (response==="success") {
-					          	//redirect user to posting feed
-					          	window.location.href = "/the-commenter/app/";
-					          }
-					          else{
-					          	$('.auth-form__message').text("Incorrect details");
-					          	if($('.auth-form__message--visible').length == 0){
-					          		$('.auth-form__message').toggleClass('auth-form__message--visible');
-					          	}
-					          }
-					          
-					       }
-					   });	
+	function validateForm(form_type, form_e){
+		switch(form_type){
+			case "login-form":
+					var userLogin = {};
+					userLogin.email = form_e.elements["email"].value;
+					userLogin.pw = form_e.elements["password"].value;
+					if(validateEmail(userLogin.email) === true){
 
+						if(userLogin.pw != ""){
+							//check pw and email in db
+							$.ajax({
+						       type: "POST",
+						       url: '../include/login-validation.php',
+						       data: {
+						       	reqType: "login",
+						       	email: userLogin.email,
+	  							password: userLogin.pw 
+						       },
+						       success: function(response)
+						       {
+						          if (response==="success") {
+						          	//redirect user to posting feed
+						          	window.location.href = "/the-commenter/app/";
+						          }
+						          else{
+						          	$('.auth-form__message').text("Incorrect details");
+						          	if($('.auth-form__message--visible').length == 0){
+						          		$('.auth-form__message').toggleClass('auth-form__message--visible');
+						          	}
+						          }
+						          
+						       }
+						   });	
+
+						}
+						
 					}
-					
-				}
-			break;
-		case "register-form":
-			console.log("To be implemented.");
-			break;
-		case "post-form":
+				break;
+			case "register-form":
+				console.log("To be implemented.");
+				break;
+			case "post-form":
 
-			break;
-		default:
-			"error";
-	}
-	return false;
-}
-
-
-function validateEmail(email) {
-	if (!email.match(/[a-zA-Z0-9._-]+[@][a-zA-Z]+\.[a-zA-Z]+/g)){
+				break;
+			default:
+				"error";
+		}
 		return false;
 	}
-	else{
-		return true;
+
+
+	function validateEmail(email) {
+		if (!email.match(/[a-zA-Z0-9._-]+[@][a-zA-Z]+\.[a-zA-Z]+/g)){
+			return false;
+		}
+		else{
+			return true;
+		}
 	}
-}
 
-if(getById('login-form') !== null) {
-	var login = getById('login-form');
-	$('#login-form').submit(function(e) {
-	    e.preventDefault(); // to stop the form from submitting
-	    if(validateForm("login-form", login) !== false){
-	    	this.submit();
-	    }
-	    else{
+	if(getById('login-form') !== null) {
+		var login = getById('login-form');
+		$('#login-form').submit(function(e) {
+		    e.preventDefault(); // to stop the form from submitting
+		    if(validateForm("login-form", login) !== false){
+		    	this.submit();
+		    }
+		    else{
 
-	    }
-	});
-}
+		    }
+		});
+	}
 
-if(getById('register-form') !== null) {
-	var register = getById('register-form');
-	$('#register-form').submit(function(e) {
-	    e.preventDefault(); // to stop the form from submitting
-	    if(validateForm("register-form", register) !== false){
-	    	this.submit();
-	    }
-	    else{
+	if(getById('register-form') !== null) {
+		var register = getById('register-form');
+		$('#register-form').submit(function(e) {
+		    e.preventDefault(); // to stop the form from submitting
+		    if(validateForm("register-form", register) !== false){
+		    	this.submit();
+		    }
+		    else{
 
-	    }
-	});
-}
+		    }
+		});
+	}
 
-
-
-
+});
